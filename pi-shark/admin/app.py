c03d4fdf8065abf777b96b8bf77adcba4e10bdac
Fulvio Cristallini <30806462+fulviocri@users.noreply.github.com>
2024-03-20 21:39:44 +0100

    v1.2

--- pi-shark/admin/app.py
+++ pi-shark/admin/app.py
@@ -1,6 +1,11 @@
+#!/pi-shark/admin/.venv/bin/python
+
 from flask import Flask
 
 app = Flask(__name__)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
 
 @app.route("/")
 def hello_world():
