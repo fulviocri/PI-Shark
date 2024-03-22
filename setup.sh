d352a546d4aa1589a270a258b5a066db2de890c8
Fulvio Cristallini <30806462+fulviocri@users.noreply.github.com>
2024-03-22 18:07:36 +0100

    v2.1

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set +e
+exec >/dev/null 2>&1
 
 if [ $(id -u) -ne 0 ]
 	then echo "Please run as root"
@@ -40,8 +41,8 @@ set_root_password() {
 		setrootpassword
 	fi
 
-	#echo "root:$root_password_1" | chpasswd -e >/dev/null 2>&1
-	echo -e "$root_password_1\n$root_password_1" | passwd root >/dev/null 2>&1
+	#echo "root:$root_password_1" | chpasswd -e
+	echo -e "$root_password_1\n$root_password_1" | passwd root
 
 	if [ $? -eq 0 ]; then
 		echo "Password changed successfully"
@@ -63,13 +64,13 @@ change_current_datetime() {
 	date
 	read -p "Is the current date and time correct? (y/n): " correct_date
 
-	if [[ $correct_date == "n" ]]; then
+	if [ $correct_date == "n" ] || [ $correct_date == "N" ]; then
 		read -p "Enter date and time (YYYY-MM-DD HH:MM:SS): " current_date
-		timedatectl set-ntp false >/dev/null 2>&1
-		timedatectl set-time "$current_date" >/dev/null 2>&1
+		timedatectl set-ntp false
+		timedatectl set-time "$current_date"
 
 		if [ $? -eq 0 ]; then
-			timedatectl set-ntp true >/dev/null 2>&1
+			timedatectl set-ntp true
 			date
 		else
 			changecurrentdatetime
@@ -88,7 +89,7 @@ set_hostname() {
 	echo "Setting host name:"
 	read -p "Type the host name: " host_name
 
-	hostnamectl set-hostname $host_name.local >/dev/null 2>&1
+	hostnamectl set-hostname $host_name.local
 
 	unset host_name
 	echo "DONE"
@@ -100,47 +101,47 @@ cleanup_system() {
 	echo ""
 	read -p "Cleaning up system. [Press enter to continue]"
 
-    if systemctl -all list-unit-files alsa-restore.service | grep "alsa-restore.service enabled" >/dev/null 2>&1 ;then
+    if systemctl -all list-unit-files alsa-restore.service | grep "alsa-restore.service enabled" ;then
 		echo "Disabling alsa-restore.service"
-	    systemctl disable alsa-restore.service >/dev/null 2>&1
-	    systemctl stop alsa-restore.service >/dev/null 2>&1
+	    systemctl disable alsa-restore.service
+	    systemctl stop alsa-restore.service
     fi
 
-    if systemctl -all list-unit-files bluetooth.service | grep "bluetooth.service enabled" >/dev/null 2>&1 ;then
+    if systemctl -all list-unit-files bluetooth.service | grep "bluetooth.service enabled" ;then
 		echo "Disabling bluetooth.service"
-	    systemctl disable bluetooth.service >/dev/null 2>&1
-	    systemctl stop bluetooth.service >/dev/null 2>&1
+	    systemctl disable bluetooth.service
+	    systemctl stop bluetooth.service
     fi
 
-    if systemctl -all list-unit-files bthelper@hci0.service | grep "bthelper@hci0.service enabled" >/dev/null 2>&1 ;then
+    if systemctl -all list-unit-files bthelper@hci0.service | grep "bthelper@hci0.service enabled" ;then
 		echo "Disabling bthelper@hci0.service"
-	    systemctl disable bthelper@hci0.service >/dev/null 2>&1
-	    systemctl stop bthelper@hci0.service >/dev/null 2>&1
+	    systemctl disable bthelper@hci0.service
+	    systemctl stop bthelper@hci0.service
     fi
 
-    if systemctl -all list-unit-files systemd-networkd-wait-online.service | grep "systemd-networkd-wait-online.service enabled" >/dev/null 2>&1 ;then
+    if systemctl -all list-unit-files systemd-networkd-wait-online.service | grep "systemd-networkd-wait-online.service enabled" ;then
 		echo "Disabling systemd-networkd-wait-online.service"
-	    systemctl disable systemd-networkd-wait-online.service >/dev/null 2>&1
-	    systemctl stop systemd-networkd-wait-online.service >/dev/null 2>&1
+	    systemctl disable systemd-networkd-wait-online.service
+	    systemctl stop systemd-networkd-wait-online.service
     fi
 
-    if systemctl -all list-unit-files triggerhappy.service | grep "triggerhappy.service enabled" >/dev/null 2>&1 ;then
+    if systemctl -all list-unit-files triggerhappy.service | grep "triggerhappy.service enabled" ;then
 		echo "Disabling triggerhappy.service"
-	    systemctl disable triggerhappy.service >/dev/null 2>&1
-	    systemctl stop triggerhappy.service >/dev/null 2>&1
+	    systemctl disable triggerhappy.service
+	    systemctl stop triggerhappy.service
 
-        systemctl disable triggerhappy.socket >/dev/null 2>&1
-        systemctl stop triggerhappy.socket >/dev/null 2>&1
+        systemctl disable triggerhappy.socket
+        systemctl stop triggerhappy.socket
     fi
 
-    if systemctl -all list-unit-files ModemManager | grep "ModemManager enabled" >/dev/null 2>&1 ;then
+    if systemctl -all list-unit-files ModemManager | grep "ModemManager enabled" ;then
 		echo "Uninstalling ModemManager and old GCC versions"
-		apt-get remove --purge -y modemmanager >/dev/null 2>&1
-		apt-get remove --purge -y gcc-7-base gcc-8-base gcc-9-base >/dev/null 2>&1
+		apt-get remove --purge -y modemmanager
+		apt-get remove --purge -y gcc-7-base gcc-8-base gcc-9-base
     fi
 
 	echo "Removing unused packages"
-	apt-get autoremove --purge -y >/dev/null 2>&1
+	apt-get autoremove --purge -y
 
 	echo "DONE"
 }
@@ -156,8 +157,8 @@ system_update() {
 	echo "Package to update: $UPDATENUM"
 
 	if [[ $UPDATENUM > 0 ]]; then
-		apt-get update >/dev/null 2>&1
-		apt-get -y upgrade >/dev/null 2>&1
+		apt-get update
+		apt-get -y upgrade
 	fi
 
 	unset UPDATENUM
@@ -170,7 +171,7 @@ install_base_component() {
 	echo ""
 	read -p "Installing base component. [Press enter to continue]"
 
-	apt-get install -y build-essential git curl xsltproc rsync tmux >/dev/null 2>&1
+	apt-get install -y build-essential git curl xsltproc rsync tmux
 
 	echo "DONE"
 }
@@ -181,14 +182,14 @@ install_network_component() {
 	echo ""
 	read -p "Installing networking component. [Press enter to continue]"
 	
-	apt-get install -y i2c-tools ufw >/dev/null 2>&1
-	apt-get install -y python3-pip python3-venv python3-smbus >/dev/null 2>&1
-	apt-get install -y nmap tcpdump doscan nast ettercap-text-only ncat >/dev/null 2>&1
-	apt-get install -y dhcpdump dhcpig dhcp-probe dhcpstarv dhcping >/dev/null 2>&1
-	# apt-get install -y arping arpon arp-scan >/dev/null 2>&1
-	apt-get install -y arping arp-scan >/dev/null 2>&1
-    apt-get install -y dnsenum dnsmap dnsrecon dnswalk dnsutils >/dev/null 2>&1
-	apt-get install -y backdoor-factory masscan netdiscover >/dev/null 2>&1
+	apt-get install -y i2c-tools ufw
+	apt-get install -y python3-pip python3-venv python3-smbus
+	apt-get install -y nmap tcpdump doscan nast ettercap-text-only ncat
+	apt-get install -y dhcpdump dhcpig dhcp-probe dhcpstarv dhcping
+	# apt-get install -y arping arpon arp-scan
+	apt-get install -y arping arp-scan
+    apt-get install -y dnsenum dnsmap dnsrecon dnswalk dnsutils
+	apt-get install -y backdoor-factory masscan netdiscover
 	
 	echo "DONE"
 }
@@ -199,17 +200,17 @@ configure_network() {
 	echo ""
 	read -p "Configuring UFW firewall. [Press enter to continue]"
 
-	rfkill unblock wifi >/dev/null 2>&1
+	rfkill unblock wifi
 
 	for filename in /var/lib/systemd/rfkill/*:wlan ; do
 		echo 0 > $filename
 	done
 
-	ufw default deny incoming >/dev/null 2>&1
-	ufw default allow outgoing >/dev/null 2>&1
-	ufw deny in on eth0 >/dev/null 2>&1
-	ufw allow in on wlan0 to any port ssh >/dev/null 2>&1
-	yes | ufw enable >/dev/null 2>&1
+	ufw default deny incoming
+	ufw default allow outgoing
+	ufw deny in on eth0
+	ufw allow in on wlan0 to any port ssh
+	yes | ufw enable
 
 	echo "DONE"
 }
@@ -220,61 +221,55 @@ copy_config_files() {
 	echo ""
 	read -p "Copy config files. [Press enter to continue]"
 
-	git clone https://github.com/fulviocri/PI-Shark.git /tmp/deploy >/dev/null 2>&1
+	git clone https://github.com/fulviocri/PI-Shark.git /tmp/deploy
 
-	rm /tmp/deploy/README.md >/dev/null 2>&1
-	rm /tmp/deploy/setup.sh >/dev/null 2>&1
+	rm /tmp/deploy/README.md
+	rm /tmp/deploy/setup.sh
 
-	cp -r /tmp/deploy/* / >/dev/null 2>&1
+	cp -r /tmp/deploy/* /
 	
-	ln -s /pi-shark/pi-shark.py /usr/bin/pi-shark . >/dev/null 2>&1
-	chown root:root /usr/bin/pi-shark . >/dev/null 2>&1
-	chmod +x /usr/bin/pi-shark . >/dev/null 2>&1
+	ln -s /pi-shark/pi-shark.py /usr/bin/pi-shark
+	chown root:root /usr/bin/pi-shark
+	chmod +x /usr/bin/pi-shark
 
-	ln -s /pi-shark/telegram-send.sh /usr/bin/telegram-send . >/dev/null 2>&1
-	chown root:root /usr/bin/telegram-send . >/dev/null 2>&1
-	chmod +x /usr/bin/telegram-send . >/dev/null 2>&1
+	ln -s /pi-shark/telegram-send.sh /usr/bin/telegram-send
+	chown root:root /usr/bin/telegram-send
+	chmod +x /usr/bin/telegram-send
 
-	ln -s /pi-shark/web-server.sh /usr/bin/web-server . >/dev/null 2>&1
-	chown root:root /usr/bin/web-server . >/dev/null 2>&1
-	chmod +x /usr/bin/web-server . >/dev/null 2>&1
-
-	echo "DONE"
-}
-
-# ========================================================================================================================================================================
-# Configuring Python vEnvironment
-python_venv() {
-	echo ""
-	read -p "Installing python vEnv and libraries. [Press enter to continue]"
-
-	python3 -m venv /pi-shark/.venv >/dev/null 2>&1
-	source /pi-shark/.venv/bin/activate >/dev/null 2>&1
-
-	pip install python-nmap >/dev/null 2>&1
-	pip install netifaces >/dev/null 2>&1
-	pip install scapy >/dev/null 2>&1
-	pip install requests >/dev/null 2>&1
-
-	deactivate >/dev/null 2>&1
+	ln -s /pi-shark/web-server.sh /usr/bin/web-server
+	chown root:root /usr/bin/web-server
+	chmod +x /usr/bin/web-server
 
 	echo "DONE"
 }
 
 # ========================================================================================================================================================================
-# Installing Django
-install_flask() {
+# Installing Python Libraries
+python_libs() {
 	echo ""
-	read -p "Installing Flask. [Press enter to continue]"
+	read -p "Installing python libraries. [Press enter to continue]"
 
-	python3 -m venv /pi-shark/admin/.venv >/dev/null 2>&1
-	source /pi-shark/admin/.venv/bin/activate >/dev/null 2>&1
-	pip install Flask
-	deactivate >/dev/null 2>&1
+	apt-get install -y python3-netifaces
+	apt-get install -y python3-requests
+	apt-get install -y python3-nmap
+	apt-get install -y python3-scapy
 
 	echo "DONE"
 }
 
+# ========================================================================================================================================================================
+# Installing Python Bottle
+install_bottle() {
+	echo ""
+	read -p "Installing Bottle. [Press enter to continue]"
+
+	python3 -m venv /pi-shark/admin/.venv
+	source /pi-shark/admin/.venv/bin/activate
+	pip install bottle
+	deactivate
+
+	echo "DONE"
+}
 
 # ========================================================================================================================================================================
 # Copy config file
@@ -299,9 +294,9 @@ change_system_locale() {
 	echo ""
 	read -p "Configuring the System Locale. [Press enter to continue]"
 
-	rm -f /etc/localtime >/dev/null 2>&1
-	echo "Europe/Rome" >/etc/timezone >/dev/null 2>&1
-	dpkg-reconfigure -f noninteractive tzdata >/dev/null 2>&1
+	rm -f /etc/localtime
+	echo "Europe/Rome" >/etc/timezone
+	dpkg-reconfigure -f noninteractive tzdata
 	dpkg-reconfigure -f noninteractive keyboard-configuration
 
 	echo "DONE"
@@ -324,8 +319,8 @@ install_base_component
 install_network_component
 configure_network
 copy_config_files
-python_venv
-install_flask
+python_libs
+#install_bottle
 customize_telegram_bot
 change_system_locale
 setup_complete
